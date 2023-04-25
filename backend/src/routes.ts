56901305184f31920d8ab910f1e0aa061bb0dcd2
Rodrigo Silva <rodrigo-cleiton@hotmail.com>
2023-04-25 01:59:40 -0300

    feat: distribuiçao da estrutura inicial do projeto

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,8 @@
-import {Router, Request, Response} from 'express'
+import {Router} from 'express'
+import { CreateUserController } from './controllers/user/CreateUserController'
 
-const router = Router()
+const routers = Router()
 
-router.get('/teste',(req: Request,res:Response)=>{
-  throw new Error('Erro ao fazer essa requisição')
-})
+routers.post('/users', new CreateUserController().handle)
 
-export {router}
+export {routers}
