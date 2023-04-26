c4e81539a928a103780cbdb1d4dccd1501ce32bd
Rodrigo Silva <rodrigo-cleiton@hotmail.com>
2023-04-25 23:02:55 -0300

    feat: criada config de login e gerar token JWT

--- backend/src/services/user/AuthUserService.ts
+++ backend/src/services/user/AuthUserService.ts
@@ -1,5 +1,6 @@
 import { compare } from "bcryptjs"
 import prismaClient from "../../prisma"
+import { sign } from "jsonwebtoken"
 
 interface AuthRequest{
   email:string,
@@ -28,8 +29,26 @@ class AuthUserService{
     }
 
     // Gerar um token JWT e devolver os dados do usuário como id, name e email
+    // Se deu td certo, gerar token usuario
+    const token = sign(
+      {
+        name: user.name,
+        email:user.email
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn:'30d'
+      }
+    )
+
     
-    return {ok:true}
+    return {
+      id:user.id,
+      name: user.name,
+      email: user.email,
+      token: token
+    }
   }
 }
 
