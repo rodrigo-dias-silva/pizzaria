0f1912b1782664cab7330a38409bcdcb44411b8a
Rodrigo Silva <rodrigo-cleiton@hotmail.com>
2023-04-25 21:31:31 -0300

    feat: criada nova rota para autenticacao/login do user

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,8 +1,12 @@
 import {Router} from 'express'
+
 import { CreateUserController } from './controllers/user/CreateUserController'
+import { AuthUserController } from './controllers/user/AuthUserController'
 
 const routers = Router()
 
 routers.post('/users', new CreateUserController().handle)
+
+routers.post('/session', new AuthUserController().handle)
 
 export {routers}
