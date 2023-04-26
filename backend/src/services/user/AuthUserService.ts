bc7767c9e91c5dc4ad066964b7c6f3ef5667c68b
Rodrigo Silva <rodrigo-cleiton@hotmail.com>
2023-04-25 22:06:44 -0300

    feat: criado arquivos para config de login e autenticaçao do user, verificando email e senha no db, senha criptografada e devidas tratativas de informaçoes erradas

---
+++ backend/src/services/user/AuthUserService.ts
@@ -0,0 +1,36 @@
+import { compare } from "bcryptjs"
+import prismaClient from "../../prisma"
+
+interface AuthRequest{
+  email:string,
+  password:string
+}
+
+class AuthUserService{
+  async execute({email,password}:AuthRequest){
+    
+    // Verificar se o email existe
+    const user = await prismaClient.user.findFirst({
+      where:{
+        email:email
+      }
+    })
+
+    if(!user){
+      throw new Error("User/password incorrect")
+    }
+
+    // Verificar senha correta
+    const passwordMatch = await compare(password, user.password)
+
+    if(!passwordMatch){
+      throw new Error("User/passord incorrect")
+    }
+
+    // Gerar um token JWT e devolver os dados do usuário como id, name e email
+    
+    return {ok:true}
+  }
+}
+
+export {AuthUserService}
