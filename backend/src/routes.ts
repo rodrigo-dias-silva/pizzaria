aa391a62ac76ede0200b7cc97dbfaa1c9bd4cd06
Rodrigo Silva <rodrigo-cleiton@hotmail.com>
2023-04-28 00:04:55 -0300

    feat: criada a nova rota de detalhes do usuario e adicionado middleware de atuenticaçao

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,11 +2,15 @@ import {Router} from 'express'
 
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
+import { DetailUserController } from './controllers/user/DetailUserController'
+import { isAuthenticated } from './middlewares/isAuthenticated'
 
 const routers = Router()
 
 routers.post('/users', new CreateUserController().handle)
 
 routers.post('/session', new AuthUserController().handle)
+
+routers.get('/user-info', isAuthenticated, new DetailUserController().handle)
 
 export {routers}
