bd270254f0dcc295e322b3d84c353d9b93eb128a
Rodrigo Silva <rodrigo-cleiton@hotmail.com>
2023-04-28 01:35:30 -0300

    feat: criado nova rota para cadastro de categorias

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,13 +4,18 @@ import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
 import { DetailUserController } from './controllers/user/DetailUserController'
 import { isAuthenticated } from './middlewares/isAuthenticated'
+import { CreateCategoryController } from './controllers/category/CreateCategoryController'
 
 const routers = Router()
 
+// Rotas user
 routers.post('/users', new CreateUserController().handle)
 
 routers.post('/session', new AuthUserController().handle)
 
 routers.get('/user-info', isAuthenticated, new DetailUserController().handle)
+
+// Rotas category
+routers.post('/category', isAuthenticated, new CreateCategoryController().handle)
 
 export {routers}
