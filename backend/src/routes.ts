1a58f2c3971d1541205389e56d4fe7ce63482abc
Rodrigo Silva <rodrigo-cleiton@hotmail.com>
2023-04-28 21:40:23 -0300

    feat: criada rota de listagem de categorias

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,21 +1,33 @@
-import {Router} from 'express'
+import { Router } from "express";
 
-import { CreateUserController } from './controllers/user/CreateUserController'
-import { AuthUserController } from './controllers/user/AuthUserController'
-import { DetailUserController } from './controllers/user/DetailUserController'
-import { isAuthenticated } from './middlewares/isAuthenticated'
-import { CreateCategoryController } from './controllers/category/CreateCategoryController'
+import { CreateUserController } from "./controllers/user/CreateUserController";
+import { AuthUserController } from "./controllers/user/AuthUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
 
-const routers = Router()
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+
+const routers = Router();
 
 // Rotas user
-routers.post('/users', new CreateUserController().handle)
+routers.post("/users", new CreateUserController().handle);
 
-routers.post('/session', new AuthUserController().handle)
+routers.post("/session", new AuthUserController().handle);
 
-routers.get('/user-info', isAuthenticated, new DetailUserController().handle)
+routers.get("/user-info", isAuthenticated, new DetailUserController().handle);
 
 // Rotas category
-routers.post('/category', isAuthenticated, new CreateCategoryController().handle)
+routers.post(
+  "/category",
+  isAuthenticated,
+  new CreateCategoryController().handle
+);
 
-export {routers}
+routers.get(
+  "/categories",
+  isAuthenticated,
+  new ListCategoryController().handle
+);
+
+export { routers };
