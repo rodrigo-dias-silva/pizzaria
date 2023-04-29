7ca6363d87af07d1e9dab06b08501e9ba5773d1a
Rodrigo Silva <rodrigo-cleiton@hotmail.com>
2023-04-29 01:06:59 -0300

    feat: criado rota de acesso as images salvas no momento do cadastro

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,29 +1,31 @@
-import express, {Request,Response,NextFunction} from 'express'
-import 'express-async-errors'
-import cors from 'cors'
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
+import cors from "cors";
+import path from "path";
 
-import { routers } from './routes'
+import { routers } from "./routes";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
+app.use(routers);
 
-app.use(routers)
+app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 
-app.use((err:Error, req:Request, res:Response, next:NextFunction)=>{
-  if(err instanceof Error){
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
     // Se for uma instancia do tipo error
     return res.status(400).json({
-      error: err.message
-    })
+      error: err.message,
+    });
   }
 
   return res.status(500).json({
-    status: 'error',
-    message: 'Internal server error.'
-  })
-})
+    status: "error",
+    message: "Internal server error.",
+  });
+});
 
-app.listen(3333,()=>console.log('Servidor Online'))
+app.listen(3333, () => console.log("Servidor Online"));
