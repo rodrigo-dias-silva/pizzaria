b967dd03da6071c658aecd168401d84cf3955a0b
Rodrigo Silva <rodrigo-cleiton@hotmail.com>
2023-05-03 03:03:40 -0300

    style: ajuste style global e config de cores padrão

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className='bg-dark-700'>
         <Main />
         <NextScript />
       </body>
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -13,6 +13,12 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'dark-900': '#101026',
+        'dark-700': '#1d1d2e',
+        'green-theme': '#3fffa3',
+        'red-theme': '#ff3f4b',
+      }
     },
   },
   plugins: [],
