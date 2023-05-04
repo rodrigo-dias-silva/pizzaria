46c161ed2e4444afe0926f5e365fc089cea4a97d
Rodrigo Linux <rodrigo-cleiton@hotmail.com>
2023-05-04 03:57:59 -0300

    feat: inicio da pagina home/login do projeto, pre estrutura da pagina

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,7 +1,30 @@
-
+import Head from 'next/head'
+import logoImg from '../../public/logo.svg'
+import Image from 'next/image'
+import { Input } from '../components/ui/Input'
 
 export default function Home() {
   return (
-    <div>DgPizzas</div>
+    <>
+      <Head>
+        <title>DG Pizza | Faça o login</title>
+      </Head>
+      <div>
+        <Image src={logoImg} alt='Logo DG Pizza' />
+
+        <div>
+          <form>
+            <Input
+              type='text'
+              placeholder='Digite seu email'
+            />
+            <Input
+              type='password'
+              placeholder='Digite sua senha'
+            />
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
