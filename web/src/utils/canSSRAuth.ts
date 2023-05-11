de4a78fadbdde5f2cd920f7fec82bf571eabad5e
Rodrigo Silva <rodrigo-cleiton@hotmail.com>
2023-05-11 04:39:16 -0300

    feat: criado config de verificacao de token do user para rotas privadas

---
+++ web/src/utils/canSSRAuth.ts
@@ -0,0 +1,42 @@
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+} from "next";
+import { destroyCookie, parseCookies } from "nookies";
+import { AuthTokenError } from "../services/errors/AuthTokenError";
+
+// func paginas só users logados podem acessar
+export function canSSRAuth<P>(fn: GetServerSideProps<P>) {
+  return async (
+    context: GetServerSidePropsContext
+  ): Promise<GetServerSidePropsResult<P>> => {
+    const cookies = parseCookies(context);
+
+    const token = cookies["@nextauth.token"];
+
+    if (!token) {
+      return {
+        redirect: {
+          destination: "/",
+          permanent: false,
+        },
+      };
+    }
+
+    try {
+      return await fn(context);
+    } catch (error) {
+      if (error instanceof AuthTokenError) {
+        destroyCookie(context, "@nextauth.token");
+
+        return {
+          redirect: {
+            destination: "/",
+            permanent: false,
+          },
+        };
+      }
+    }
+  };
+}
