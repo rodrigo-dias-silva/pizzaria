381637446d57d7188fc1c8c72868b0288406ca72
Rodrigo Silva <rodrigo-cleiton@hotmail.com>
2023-05-25 10:38:00 -0300

    fix: separacao tipo de botoes

--- web/src/components/ui/Button/index.tsx
+++ web/src/components/ui/Button/index.tsx
@@ -4,14 +4,12 @@ import { ButtonHTMLAttributes, ReactNode } from "react"
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   loading?: boolean,
   children: ReactNode,
-  bgcolor: string,
-  txtcolor: string
 }
 
-export function Button({ loading, children, bgcolor, txtcolor, ...rest }: Props) {
+function ButtonRed({ loading, children, ...rest }: Props) {
   return (
     <button
-      className={`w-full h-10 bg-${bgcolor}-theme flex justify-center items-center p-2 text-${txtcolor} font-semibold text-xl rounded-lg transition-all hover:brightness-125 disabled:cursor-not-allowed`}
+      className='w-full h-10 bg-red-theme flex justify-center items-center p-2 text-white font-semibold text-xl rounded-lg transition-all hover:brightness-125 disabled:cursor-not-allowed'
       disabled={loading}
       {...rest}
     >
@@ -19,3 +17,17 @@ export function Button({ loading, children, bgcolor, txtcolor, ...rest }: Props)
     </button>
   )
 }
+
+function ButtonGreen({ loading, children, ...rest }: Props) {
+  return (
+    <button
+      className='w-full h-10 bg-green-theme flex justify-center items-center p-2 text-black font-semibold text-xl rounded-lg transition-all hover:brightness-125 disabled:cursor-not-allowed'
+      disabled={loading}
+      {...rest}
+    >
+      {loading ? (<SpinnerGap size={16} className="animate-spin" />) : (children)}
+    </button>
+  )
+}
+
+export { ButtonGreen, ButtonRed }
