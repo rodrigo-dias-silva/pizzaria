73f3b5cd383cc1e9ca057cd421443c02d8613e24
Rodrigo Silva <rodrigo-cleiton@hotmail.com>
2023-05-26 14:40:03 -0300

    feat: criado componente modal e configurado

--- web/src/components/ModalOrder/index.tsx
+++ web/src/components/ModalOrder/index.tsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { OrderItemProps } from '@/src/pages/dashboard'
+import { X } from '@phosphor-icons/react'
+import Modal from 'react-modal'
 
-type Props = {}
+type ModalOrderProps = {
+  isOpen: boolean,
+  onRequestClose: () => void,
+  order: OrderItemProps[]
+}
 
-export default function ModalOrder({ }: Props) {
+export default function ModalOrder({ isOpen, onRequestClose, order }: ModalOrderProps) {
+
+  const customStyles = {
+    content: {
+      top: '50%',
+      bottom: 'auto',
+      left: '50%',
+      right: 'auto',
+      padding: '30px',
+      transform: 'translate(-50%,-50%)',
+      backgroundColor: '#1d1d2e'
+    }
+  }
+
   return (
-    <div>ModalOrder</div>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      style={customStyles}
+    >
+      <button
+        type='button'
+        onClick={onRequestClose}
+        className='react-modal-close'
+      >
+        <X size={40} color='#f34748' />
+      </button>
+
+      <div>
+        <h2>Detalhes do pedido</h2>
+        <span>Mesa:<strong>{order[0].order.table}</strong></span>
+
+        {order.map(item => (
+          <section
+            key={item.id}
+            className=''
+          >
+            <span>{item.amount} - <strong>{item.product.name}</strong></span>
+            <span>{item.product.description}</span>
+          </section>
+        ))}
+      </div>
+
+    </Modal>
   )
 }
