3bee0e2954f93e36e64cb24544eb317a4203ce43
Rodrigo Silva <rodrigo-cleiton@hotmail.com>
2023-05-26 15:45:12 -0300

    style: estilizacao do modal e responsividade

--- web/src/components/ModalOrder/index.tsx
+++ web/src/components/ModalOrder/index.tsx
@@ -31,24 +31,30 @@ export default function ModalOrder({ isOpen, onRequestClose, order }: ModalOrder
       <button
         type='button'
         onClick={onRequestClose}
-        className='react-modal-close'
+        className='react-modal-close absolute right-4 top-4'
       >
         <X size={40} color='#f34748' />
       </button>
 
-      <div>
-        <h2>Detalhes do pedido</h2>
-        <span>Mesa:<strong>{order[0].order.table}</strong></span>
+      <div className='max-sm:w-96 max-[500px]:w-80 w-[580px] mt-10 bg-dark-700 text-zinc-400'>
+        <h2 className='my-4 text-2xl font-bold text-white'>Detalhes do pedido</h2>
+        <span className='text-lg'>Mesa: <strong className='text-white'>{order[0].order.table}</strong></span>
 
         {order.map(item => (
           <section
             key={item.id}
-            className=''
+            className='flex flex-col my-4'
           >
-            <span>{item.amount} - <strong>{item.product.name}</strong></span>
-            <span>{item.product.description}</span>
+            <span className='text-white'>{item.amount} - <strong>{item.product.name}</strong></span>
+            <span className='break-all'>{item.product.description}</span>
           </section>
         ))}
+
+        <button
+          className='mt-6 bg-dark-900 py-2 px-4 rounded text-green-theme font-semibold'
+        >
+          Concluir pedido
+        </button>
       </div>
 
     </Modal>
