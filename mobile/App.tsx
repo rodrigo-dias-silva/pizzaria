9a84e93ac4b03cce6a025d862647f98c0ac33898
Rodrigo Silva <rodrigo-cleiton@hotmail.com>
2023-06-02 16:23:39 -0300

    feat: alteracao statusbar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StatusBar, View } from 'react-native';
+import SignIn from './src/pages/SignIn';
 
 export default function App() {
   return (
-    <View className='flex-1 items-center justify-center bg-zinc-900'>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className='flex-1 items-center justify-center bg-dark-700'>
+      <StatusBar backgroundColor='#1d1d2e' barStyle='light-content' translucent={false} />
+      <SignIn />
     </View>
   );
 }
