597ee317cfc3e1f262068bb67a5c6aab1249dbe7
Rodrigo Silva <rodrigo-cleiton@hotmail.com>
2023-06-02 16:22:43 -0300

    feat: config padroes cores

--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./App.{ts,tsx}", "./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-900': '#101026',
+        'dark-700': '#1d1d2e',
+        'green-theme': '#3fffa3',
+        'red-theme': '#ff3f4b',
+      }
+    },
   },
   plugins: [],
 }
