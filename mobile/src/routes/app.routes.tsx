de01868ce1502263394ba1100e5f66486759837b
Rodrigo Silva <rodrigo-cleiton@hotmail.com>
2023-06-08 07:53:33 -0300

    feat: criado arq rota de usuarios logados

---
+++ mobile/src/routes/app.routes.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import Dashboard from '../pages/Dashboard'
+
+const Stack = createNativeStackNavigator()
+
+export default function AppRoutes() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name='Dashboard' component={Dashboard} />
+    </Stack.Navigator>
+  )
+}
