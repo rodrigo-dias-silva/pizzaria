54a0957872686ad0602cfc842e9065de77044d20
Rodrigo Silva <rodrigo-cleiton@hotmail.com>
2023-06-28 00:27:34 -0300

    feat: add ActivityIndicator to the loading state

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -2,11 +2,20 @@
 import React from 'react'
 import AuthRoutes from './auth.routes'
 import AppRoutes from './app.routes'
+import { ActivityIndicator, View } from 'react-native'
 
 export default function Routes() {
 
   const isAuthenticated = false
   const loading = false
+
+  if (loading) {
+    return (
+      <View className='flex-1 bg-dark-700 justify-center items-center'>
+        <ActivityIndicator size={60} color='#f5f7fb' />
+      </View>
+    )
+  }
 
   return (
     isAuthenticated ?
