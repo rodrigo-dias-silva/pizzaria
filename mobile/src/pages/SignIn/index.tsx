9e9006879c79f160171d59a15ebd4f64a3852069
Rodrigo Silva <rodrigo-cleiton@hotmail.com>
2023-06-28 00:24:59 -0300

    feat: create the structure and styling of the SignIn page

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { View, Image } from 'react-native'
+
+import InputText from '../../components/InputText'
+import { ButtonGreen } from '../../components/Button'
+
 
 export default function SignIn() {
   return (
-    <View>
-      <Text>Tela de login</Text>
+    <View className='flex-1 justify-center items-center bg-dark-700'>
+      <Image
+        source={require('../../assets/logo.png')}
+        className='mb-5'
+      />
+
+      <View className='w-full px-5 items-center justify-center space-y-3'>
+        <InputText
+          placeholder='Digite seu e-mail'
+          bgColor='dark-900'
+        />
+
+        <InputText
+          placeholder='Digite sua senha'
+          bgColor='dark-900'
+          secureTextEntry={true}
+        />
+
+        <ButtonGreen
+          content='Acessar'
+        />
+      </View>
     </View>
   )
 }
