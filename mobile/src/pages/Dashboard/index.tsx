879f9faa8c3e4344dbb90552b7c377e7cfec1223
Rodrigo Silva <rodrigo-cleiton@hotmail.com>
2023-07-11 04:51:52 -0300

    feat: temporary button created to logout

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,10 +1,21 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
+import { ButtonRed } from '../../components/Button'
 
 export default function Dashboard() {
+
+  const { signOut } = useContext(AuthContext)
+
   return (
     <View>
       <Text>Dashboard</Text>
+
+      <ButtonRed
+        content='Sair do App'
+        onPress={signOut}
+      />
+
     </View>
   )
 }
