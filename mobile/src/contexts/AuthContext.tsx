0c87fd643b52371a2b91ebd195937cef89727234
Rodrigo Silva <rodrigo-cleiton@hotmail.com>
2023-07-11 03:33:12 -0300

    feat: create SignIn function with types, loadingAuth states, asyncStorage configuration and api callback

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,8 +1,12 @@
 import { ReactNode, createContext, useState } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+import { api } from '../services/api'
 
 type AuthContextData = {
   user: UserProps,
-  isAuthenticated: boolean
+  isAuthenticated: boolean,
+  signIn: (credentials: SignInProps) => Promise<void>
 }
 
 type UserProps = {
@@ -14,6 +18,11 @@ type UserProps = {
 
 type AuthProviderProps = {
   children: ReactNode
+}
+
+type SignInProps = {
+  email: string,
+  password: string
 }
 
 export const AuthContext = createContext({} as AuthContextData)
@@ -29,8 +38,46 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
   const isAuthenticated = !!user.name
 
+  const [loadingAuth, setLoadingAuth] = useState(false)
+
+  async function signIn({ email, password }: SignInProps) {
+
+    setLoadingAuth(true)
+
+    try {
+      const response = await api.post('/session', {
+        email,
+        password
+      })
+
+      const { id, name, token } = response.data
+
+      const data = {
+        ...response.data
+      }
+
+      await AsyncStorage.setItem('@app_pizzaria', JSON.stringify(data))
+
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+
+      setUser({
+        email,
+        id,
+        name,
+        token
+      })
+
+      setLoadingAuth(false)
+
+    } catch (err) {
+      console.error('erro ao acessar, erro: ' + err);
+      setLoadingAuth(false)
+    }
+
+  }
+
   return (
-    <AuthContext.Provider value={{ user, isAuthenticated }}>
+    <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
       {children}
     </AuthContext.Provider>
   )
