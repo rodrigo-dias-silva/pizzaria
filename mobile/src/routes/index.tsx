36b6e12e1f793fe57a5805c727efc6d6e11794b4
Rodrigo Silva <rodrigo-cleiton@hotmail.com>
2023-07-11 04:43:03 -0300

    feat: import loading state from AuthContext

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -9,8 +9,7 @@ import { AuthContext } from '../contexts/AuthContext'
 
 export default function Routes() {
 
-  const { isAuthenticated } = useContext(AuthContext)
-  const loading = false
+  const { isAuthenticated, loading } = useContext(AuthContext)
 
   if (loading) {
     return (
