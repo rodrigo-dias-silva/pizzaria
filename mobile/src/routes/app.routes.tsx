3292ded60b19e2bfb51d8471cd1efdabf1d6fa04
Rodrigo Silva <rodrigo-cleiton@hotmail.com>
2023-07-12 05:59:40 -0300

    feat: header navigation removal

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -8,7 +8,7 @@ const Stack = createNativeStackNavigator()
 export default function AppRoutes() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name='Dashboard' component={Dashboard} />
+      <Stack.Screen name='Dashboard' component={Dashboard} options={{ headerShown: false }} />
     </Stack.Navigator>
   )
 }
