69474f47b67e9b2b17a1bc94bc2e0d7b8d52cd9a
Rodrigo Silva <rodrigo-cleiton@hotmail.com>
2023-07-23 12:05:37 -0300

    feat: created method to receive params

--- mobile/src/pages/Order/index.tsx
+++ mobile/src/pages/Order/index.tsx
@@ -1,10 +1,27 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+import { useRoute, RouteProp } from '@react-navigation/native'
+
+type RouteDetailParms = {
+  Order: {
+    number: string | number,
+    order_id: string
+  }
+}
+
+type OrderRouteProps = RouteProp<RouteDetailParms, 'Order'>
+
 export default function Order() {
+
+  const route = useRoute<OrderRouteProps>()
+
   return (
     <View>
       <Text>Order</Text>
+      <Text>
+        {route.params.number}
+      </Text>
     </View>
   )
 }
