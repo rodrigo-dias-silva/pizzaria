b247c0d2a5df5730d82f107742af4ffb891ddf08
Rodrigo Silva <rodrigo-cleiton@hotmail.com>
2023-08-08 03:31:52 -0300

    feat: created function to delete order

--- mobile/src/pages/Order/index.tsx
+++ mobile/src/pages/Order/index.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 
-import { useRoute, RouteProp } from '@react-navigation/native'
+import { useRoute, RouteProp, useNavigation } from '@react-navigation/native'
 import { InputTextCenter } from '../../components/InputText'
 import { ButtonBlue, ButtonGreen } from '../../components/Button'
+
+import { api } from '../../services/api'
 
 type RouteDetailParms = {
   Order: {
@@ -18,6 +20,23 @@ type OrderRouteProps = RouteProp<RouteDetailParms, 'Order'>
 export default function Order() {
 
   const route = useRoute<OrderRouteProps>()
+  const navigation = useNavigation()
+
+  async function handleDeleteOrder() {
+    try {
+
+      await api.delete('/order', {
+        params: {
+          order_id: route.params?.order_id
+        }
+      })
+
+      navigation.goBack()
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <SafeAreaView className='flex-1 py-4 bg-dark-700 px-5 space-y-6'>
@@ -25,7 +44,7 @@ export default function Order() {
         <Text className='text-3xl font-bold text-white mr-4'>
           Mesa {route.params.number}
         </Text>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={handleDeleteOrder}>
           <Feather name='trash-2' size={28} color='#ff3f4b' />
         </TouchableOpacity>
       </View>
